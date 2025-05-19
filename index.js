29310e0636e7701ea496784cbaf959d580ff0c0f
NP237-DEV <ntumbourprecious@gmail.com>
2025-05-19 23:36:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname)); // Serve static files like index.html
+const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const streamifier = require('streamifier');
